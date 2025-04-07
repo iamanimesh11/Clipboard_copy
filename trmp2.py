f058be87c0a3ac32731f21f6c8035e260cd4d073
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-04-07 11:06:17 +0530

    Update trmp2.py

--- trmp2.py
+++ trmp2.py
@@ -1,25 +1,95 @@
-# airflow/dags/kafka_road_producer_dag.py
+import time
+import os
+import sys
+import json
+import string
+import random
 
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from datetime import datetime, timedelta
 
-default_args = {
-    'owner': 'animesh',
-    'retries': 2,
-    'retry_delay': timedelta(minutes=1),
-}
+from kafka import KafkaProducer
+from datetime import datetime
+from kafka.admin import KafkaAdminClient,NewTopic
+from oauthlib.common import generate_timestamp
 
-with DAG(
-    dag_id='kafka_road_producer_dag',
-    default_args=default_args,
-    description='DAG to run Kafka road producer script',
-    schedule_interval='*/5 * * * *',  # Every 5 minutes
-    start_date=datetime(2024, 4, 7),
-    catchup=False,
-) as dag:
+AIRFLOW_HOME = os.getenv("AIRFLOW_HOME", "/opt/airflow")
+if AIRFLOW_HOME != "/opt/airflow":
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    AIRFLOW_HOME = BASE_DIR
+    print(BASE_DIR)
+    
+SCRIPTS_PATH = os.path.join(AIRFLOW_HOME, "scripts")
+sys.path.append(SCRIPTS_PATH)
+common_PATH = os.path.join(AIRFLOW_HOME, "common") 
+sys.path.append(common_PATH)
+common_PATH = os.path.join(AIRFLOW_HOME, "common","logging_and_monitoring","logs") 
+sys.path.append(common_PATH)
 
-    run_kafka_road_producer = BashOperator(
-        task_id='run_kafka_road_producer',
-        bash_command='python3 /path/to/kafka-road-producer.py',
-    )
+from utils.api_utils import get_nearby_roads_with_coordinates
+
+#from road_data_real_time_traffic_onbounding_box import get_nearby_roads_with_coordinates
+from modify_Topics import create_kafka_topic
+from logging_and_monitoring.centralized_logging import setup_logger
+
+producer_Script_logs_path=os.path.join(common_PATH, "Road_Producer.log")
+logger = setup_logger("Road_Data_ETL", "producer_script", "kafka", producer_Script_logs_path)
+
+admin_client=KafkaAdminClient(
+            bootstrap_servers=['kafka:9092'],
+            client_id="kafka_topic_manager"
+        )
+
+def producer():
+    logger.info("Producer funtion called")
+    try:
+        producer = KafkaProducer(
+            bootstrap_servers='kafka:9092',
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+        logger.info("Producer passed")
+
+
+    except Exception as e:
+        print(f"Error occurred:{e}")
+        logger.error("Error occured : {e}")
+        exit()
+    road_list = []
+    nearby_roads = get_nearby_roads_with_coordinates(28.60507059568563, 77.44698311030206)
+    print(len(nearby_roads))
+   
+    logger.info(len(nearby_roads))
+    if nearby_roads:
+        # Print results
+        for road_name, coords in nearby_roads.items():
+            # logging.info(f"Road_name: {road_name}, Start_coords : {coords['start']} , End_coords : {coords['end']}")
+            road_list.append({
+                "name": road_name,
+                "start_lat": coords['start'][0],
+                "start_lon": coords['start'][1],
+                "end_lat": coords['end'][0],
+                "end_lon": coords['end'][1],
+            })
+        timestamp=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        message = json.dumps(road_list).encode('utf-8')
+        logger.info(message)
+
+        try:
+                f = producer.send('road-topic',value=road_list)
+                f.get(timeout=10)
+                logger.info("Message sent")
+                time.sleep(3)
+
+        except Exception as e:
+                logger.fatal(f"failed to send message {e}")
+
+        producer.close()
+
+
+if __name__ == "__main__":
+    try: 
+        create_kafka_topic(admin_client,"road-topic",2,1)
+        logger.info(f"topic road-topic created successfully")
+
+    except Exception as e:
+        logger.fatal(f"failed to create topic road-topic: {e}")
+
+    producer()
