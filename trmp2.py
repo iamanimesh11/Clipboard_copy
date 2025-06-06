7ccff64a957cefd79871e702b11d89697317d476
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-06-06 11:05:52 +0530

    Update trmp2.py

--- trmp2.py
+++ trmp2.py
@@ -1,18 +1,53 @@
-# run_device_type_consumers.py
+🚀 Project Drop!
+Long break but After 2–3 months of learning, building, debugging, and Lots of coffee☕… I’m here to share:
 
-import json
-import subprocess
+“Real-Time ETL Roads Traffic & Weather Monitoring ” — a complete Data Engineering powerhouse Project !
 
-with open("device_types.json") as f:
-    device_types = json.load(f)["device_types"]
+🛣️🌦️ What’s it all about?
+This system captures live road, traffic, and weather data using APIs, processes it through Apache Airflow & Kafka, stores it in PostgreSQL, and monitors logs everything with Grafana Loki — all running seamlessly in Docker containers!
 
-for device in device_types:
-    container_name = f"mqtt_consumer_{device}"
-    subprocess.run([
-        "docker", "run", "-d",
-        "--name", container_name,
-        "--network", "host",  # So it can access MQTT broker locally
-        "-e", f"DEVICE_TYPE={device}",
-        "-e", "MQTT_BROKER=localhost",
-        "mqtt_consumer_image"  # name of image you build from Dockerfile
-    ])
+✨ Just docker compose up -d --build and you're good to go!🔥
+
+⚙️ Built With:
+
+🐍 Python as the core language
+📡 TomTom, Overpass & Weather APIs
+⏰ Apache Airflow for ETL orchestration
+🔄 Apache Kafka for real-time data streaming
+🧱 PostgreSQL for structured storage
+📊 Grafana Loki + Promtail for centralized logging
+🧪 Custom Streamlit UI for guiding ETL helpers
+🔐 API Keys and secrets stored securely with Firebase
+🔔 Discord Webhooks for error & process alerts
+
+📦 Everything is production-inspired:
+Retries, fallbacks, modular configs, Docker volumes, and clean logging built for scale!
+
+🎯 What You Can Explore:
+
+Project website :https://lnkd.in/g45RQMKU
+✅ Live API Health Check before any GitHub project cloning 
+
+🖼️ Gallery preview to see snapshots of services used in project 
+
+--------
+
+📡 Real-time data pipeline in action – with Streamlit UI App runs in container.
+
+🧱 Modular project structure with clean configs and secure API handling
+
+📽️ Watch the full architecture workflow in the attached video
+
+
+🗂️ Fully documented GitHub repository ready for your review and feedback!
+
+
+👉 GitHub Repo: 
+https://lnkd.in/g_cHN3kx.
+
+(Show it some ⭐️ if you find it valuable!)
+
+So, Go ahead with Readme file now.
+---
+
+This isn’t just a portfolio project — it’s a real-world data pipeline showcasing my skills ♥️ in streaming, orchestration, logging, and system design.
