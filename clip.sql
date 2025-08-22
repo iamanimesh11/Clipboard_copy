57ad1fcbe8a0b3d36bce0d74501f9fb690fcddce
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-08-22 11:49:33 +0530

    Update clip.sql

--- clip.sql
+++ clip.sql
@@ -1,32 +1,22 @@
+CREATE OR REPLACE PROCEDURE Practice.sp_check_pageviews(IN in_suffix string ,IN  min_pageviews int64,out out_msg string)
+BEGIN 
 
--- BigQuery script example
-BEGIN
-
-  CREATE TEMP TABLE demo_table (
-    totals STRUCT<pageviews INT64, transactions INT64>,
-    tags ARRAY<STRING>,
-    hits ARRAY<STRUCT<pagePath STRING, timeOnPage INT64>>
-  );
-
-  INSERT INTO demo_table (totals, tags, hits)
-  VALUES
-  (
-    STRUCT(5 AS pageviews, 2 AS transactions),
-    ['analytics', 'bigquery', 'demo'],
-    [STRUCT('/home' AS pagePath, 10 AS timeOnPage),
-     STRUCT('/about' AS pagePath, 5 AS timeOnPage)]
-  ),
-  (
-    STRUCT(12 AS pageviews, 0 AS transactions),
-    ['cloud', 'sql'],
-    [STRUCT('/pricing' AS pagePath, 8 AS timeOnPage)]
-  ),
-  (
-    STRUCT(0 AS pageviews, 0 AS transactions),
-    [],  -- empty array
-    []   -- empty array of structs
-  );
-
-  SELECT * FROM demo_table;
-
+  DECLARE sql STRING;
+  DECLARE n int64 default 0;
+  SET sql='''
+  SELECT count(totals.pageviews)  as count_pageViews from `bigquery-public-data.google_analytics_sample.ga_sessions_*` where _TABLE_SUFFIX =@suffix
+''' ; 
+  Execute IMMEDIATE SQL INTO n using in_suffix as suffix;
+  if n =0 then 
+  set out_msg="No data for this date";
+  elseif n<min_pageviews then
+    set out_msg=FORMAT("low activity:%d Pageviews",n);
+  else 
+    set out_msg=FORMAT("good traffic %d Pageviews",n);
+  
+  end if ;
 END;
+
+DECLARE nt string;
+CALL  Practice.sp_check_pageviews('20170801',5,nt);
+select nt;
