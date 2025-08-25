d1e3415c271041306c0c401553cd104d2089856a
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-08-25 14:45:02 +0530

    Update clip.sql

--- clip.sql
+++ clip.sql
@@ -1,18 +1,19 @@
-Below query doesnt show any data to display;
+damn it . now i have made copied your code exactly and still its showing no data to display whne run select query:
+CREATE OR REPLACE TABLE Practice.ga_Sessions_partitioned_temp as
+  SELECT fullvisitorId,
+          visitStartTime,
+          totals.pageviews as pv,
+          hits.page.pagePAth as pagePAth,
+          trafficSource.source AS trafficsource,
+          DATE(TIMESTAMP_SECONDS(visitStartTime)) as visit_date
+  from   `bigquery-public-data.google_analytics_sample.ga_sessions_*`, UNNEST(hits) AS hits
+  WHERE _TABLE_SUFFIX BETWEEN '20170801' AND '20170831' and visitStartTIme IS NOT NULL;
 
-SELECT fullVisitorId FROM Practice.ga_Sessions_partitioned
-WHERE visit_date = '2017-08-01'
+
+CREATE OR REPLACE TABLE Practice.ga_Sessions_partitioned PARTITION BY visit_date CLUSTER BY fullVisitorId,trafficsource as 
+select * from Practice.ga_Sessions_partitioned_temp
+
+
+select  fullvisitorId from Practice.ga_Sessions_partitioned
+where visit_date='2017-08-01'
 LIMIT 10;
-
-
-but when i query the main public dataset its shows output as expected .
-SELECT 
-    fullVisitorId
-FROM 
-    `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
-    UNNEST(hits) AS hits
-WHERE 
-    _TABLE_SUFFIX = '20170801';
-
-
-i'm getting frustureated for real
