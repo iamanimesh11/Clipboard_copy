53494d7f9f1b0ad77855360e9b83bade0b4ef2e1
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-08-25 11:18:23 +0530

    Update clip.sql

--- clip.sql
+++ clip.sql
@@ -1,11 +1,1 @@
-getting error:Cannot access field source on a value with type STRING at [1:150]
-
-CREATE OR REPLACE TABLE Practice.ga_Sessions_partitioned PARTITION BY DATE(TIMESTAMP_SECONDS(visitStartTime)) CLUSTER BY fullVisitorId,trafficSource.source
-AS 
-  SELECT fullvisitorId,
-          visitStartTime,
-          totals.pageviews,
-          hits.page.pagePAth as pagePAth,
-          trafficSource.source AS trafficsource
-  from   `bigquery-public-data.google_analytics_sample.ga_sessions_*`, UNNEST(hits) AS hits
-  WHERE _TABLE_SUFFIX BETWEEN '20170801' AND '20170831';
+nw it says: PARTITION BY expression must be DATE(<timestamp_column>), DATE(<datetime_column>), DATETIME_TRUNC(<datetime_column>, DAY/HOUR/MONTH/YEAR), a DATE column, TIMESTAMP_TRUNC(<timestamp_column>, DAY/HOUR/MONTH/YEAR), DATE_TRUNC(<date_column>, MONTH/YEAR), or RANGE_BUCKET(<int64_column>, GENERATE_ARRAY(<int64_value>, <int64_value>[, <int64_value>]))
