a2860b36b1a09f18c996cc47b3d4d643ba2a32ac
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-08-25 13:55:14 +0530

    Update clip.sql

--- clip.sql
+++ clip.sql
@@ -1,12 +1,8 @@
 one thing i have noticed :when i create table normaly without partion by and cluster by clause then data got inserted into it successfully :
 CREATE OR REPLACE TABLE Practice.ga_Sessions_partitioned_temp
 
-but when i did CREATE OR REPLACE TABLE Practice.ga_Sessions_partitioned_temp2 PARTITION BY visit_date as
-  SELECT fullvisitorId,
-          visitStartTime,
-          totals.pageviews as pv,
-          hits.page.pagePAth as pagePAth,
-          trafficSource.source AS trafficsource,
+but when i did CREATE OR REPLACE TABLE Practice.ga_Sessions_partitioned_temp2 PARTITION BY visit_date CLUSTER BY fullVisitorId,trafficsource as
+  SELECT fullvisitorId,visitStartTime,totals.pageviews as pv,hits.page.pagePAth as pagePAth,trafficSource.source AS trafficsource,
           DATE(TIMESTAMP_SECONDS(visitStartTime)) as visit_date
   from   `bigquery-public-data.google_analytics_sample.ga_sessions_*`, UNNEST(hits) AS hits
   WHERE _TABLE_SUFFIX BETWEEN '20170801' AND '20170831';
@@ -15,5 +11,5 @@ but when i did CREATE OR REPLACE TABLE Practice.ga_Sessions_partitioned_temp2 PA
 table created as well but  problem is when i do  run
 select count(*) from Practice.ga_Sessions_partitioned_temp2 ;
 
-it shows count as 0  ,whats happening ,are you able to understand
-why its happening as data is already there in that date range ,
+it SHOWS:This query will process 0 B when run.
+BUT WHEN EXECUTED IT SHOWS COUNT AS :13233
