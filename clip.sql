e7e1b6760d338314d7b348179ca17762cc38e626
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-08-28 13:23:43 +0530

    Update clip.sql]

--- clip.sql
+++ clip.sql
@@ -1,70 +1,24 @@
-Dim sb As New System.Text.StringBuilder()
+I wanto to direclty mail to email anuradha.seereddy@skf.com . to show my interest instead of applying ,in case if it works .plz write a mail accordingly on below decsription.
 
-' Start table with class
-sb.Append("<table class='custom-table'>")
+ Data Engineer, India – Automobile Business 
 
-' Add table headers with conditional formatting
-Dim headerClassMap As New Dictionary(Of String, String)() ' Stores the header class for each column by name
+Design, build, and maintain the data infrastructure and systems that support SKF VA data needs. By leveraging their skills in data modeling, data integration, data processing, data storage, data retrieval, and performance optimization, this role can help VA manage and utilize their data more effectively.
+Key responsiblities (or What you can expect in the role)
 
-sb.Append("<tr class='header-row'>")
-For Each col As System.Data.DataColumn In dtData.Columns
-    Dim headerText As String = col.ColumnName
-    Dim headerClass As String = ""
-    Dim headerTextLower As String = headerText.ToLower() ' Convert to lowercase once for efficiency
+ Build an VA data warehouse which is scalable, secured, and compliant using snowflake technologies. This would include designing and developing Snowflake data models 
+ Work with Central data warehouse like SDW, MDW, OIDW to extract data and enrich with VA specific customer grouping, program details etc. 
+ Data integration: Responsible for integrating data from ERP’s, BPC and other systems into Snowflake, SKF standard DW’s ensuring that data is accurate, complete, and consistent. 
+ Performance optimization: Responsible for optimizing the performance of Snowflake queries and data loading processes. Involves optimizing SQL queries, creating indexes, and tuning data loading processes. 
+ Security and access management: Responsible for managing the security and access controls of the Snowflake environment. This includes configuring user roles and permissions, managing encryption keys, and monitoring access logs. 
+ Maintain existing databases, warehouse solutions addressing support needs, enhancements Troubleshooting etc. 
 
-    ' Check the conditions for coloring
-    If headerTextLower.Contains("full") AndAlso headerTextLower.Contains("pm") AndAlso headerTextLower.Contains("plan") Then
-        headerClass = "header-yellow"
-    ElseIf headerTextLower.Contains("light") AndAlso headerTextLower.Contains("pm") AndAlso headerTextLower.Contains("plan") AndAlso Not headerTextLower.Contains("red") Then
-        headerClass = "header-yellow"
-    ElseIf headerTextLower.Contains("red") Then
-        headerClass = "header-red"
-    ElseIf headerTextLower.Contains("lg support manager") Then
-        headerClass = "header-yellow"
-    Else
-        headerClass = "header-grey"
-    End If
-
-    sb.Append("<th class='" & headerClass & " header-centre'>" & headerText & "</th>")
- If headerTextLower.Contains("count") Then
-        headerClass = "header-red"
-	End If
-    ' Store the header class for this column
-    headerClassMap.Add(headerText, headerClass)
-Next
-sb.Append("</tr>")
-
-' Add table rows with alternating colors and conditional formatting for Status
-Dim rowIndex As Integer = 0
-
-For Each row As DataRow In dtData.Rows
-    Dim rowClass As String = If(rowIndex Mod 2 = 0, "even-row", "odd-row")
-    sb.Append("<tr class='" & rowClass & "'>")
-
-    For Each col As System.Data.DataColumn In dtData.Columns
-    Dim cellValue As String = row(col.ColumnName).ToString()
-    Dim cellClass As String = ""
-
- 
-
- If rowIndex=dtDATA.Rows.Count -1 Then 
-		cellClass &= " bold_txt"
-		
-	End If 
-	
-
-    ' Center-align numeric values
-    If IsNumeric(cellValue)Then
-        cellClass &= " cell-center"
-    End If
-
-
-    sb.Append("<td class='" & cellClass.Trim() & "'>" & cellValue & "</td>")
-Next
-
-    sb.Append("</tr>")
-    rowIndex += 1
-Next
-
-sb.Append("</table>")
-htmlTable = sb.ToString()
+ Competencies 
+ Should have a good understanding of data modeling concepts and should be familiar with Snowflake's data modeling tools and techniques. 
+ SQL: Should be expert in SQL. Should be able to write complex SQL queries and understand how to optimize SQL performance in Snowflake. 
+ Pipeline Management & ETL: Should be able to design and manage data pipelines on Snowflake and Azure, using ETL/ELT tools (e.g., DBT, Alteryx, Talend, Informatica). 
+ Should have a good understanding of cloud computing concepts and be familiar with the cloud infrastructure on which Snowflake operates. 
+ Good understanding of data warehousing concepts and be familiar with Snowflake's data warehousing tools and techniques 
+ Familiar with data governance and security concepts 
+ Able to identify and troubleshoot issues with Snowflake and SKF’s data infrastructure 
+ Experience with Agile solution development 
+ Good to have – knowledge on SKF ERP systems (XA, SAP, PIM etc.), data related sales, supply chain data, manufacturing. 
