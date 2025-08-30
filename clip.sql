a13538fd8244d8f617cea3a64aecf78946cbdda6
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-08-30 13:16:07 +0530

    Update clip.sql

--- clip.sql
+++ clip.sql
@@ -1,15 +1,15 @@
 NEUHEIT TECHNOLOGIES
 803, Tyagi Nagar, Swarag Ashram Road, Hapur 245101
-Pay Slip for July-25
+Pay Slip for February-25
 Name of the Employee Animesh Singh UAN 102073356695
 Employee ID NT/2024/01 PF No N.A.
-Designation Jr. Developer Bank Name HDFC Bank
-Department IT Bank A/C No 50100771691246
-DOJ 01-04-2024 Paid Days 31
+Designation Jr. Developer Bank Name Federal Bank
+Department IT Bank A/C No 77770115219913
+DOJ 01-04-2024 Paid Days 28
 Earnings Deductions
-Basic Wage ₹20,700 EPF ₹2,484
-HRA ₹10,350
-Other Allowances ₹16,305
-Total Earnings ₹47,355 Total Deductions ₹2,484
-Net Salary ₹44,871
-Employer Signature Employee Signature
+Basic Wage ₹15,000 EPF ₹1,800
+HRA ₹6,000
+Other Allowances ₹18,867
+Total Earnings ₹39,867 Total Deductions ₹1,800
+Net Salary ₹38,067
+Employer Signature Employee Signa
