b1adcd9b5383c45885ece344f15801ca1efe8c66
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-09-04 14:59:34 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,15 +1,11 @@
-. [1222040] Continent with third higher Hunger affected population percentage
+Dear Animesh
 
-You have been provided following data by the WHO to understand the details of world hunger.
 
-Table: World_hunger_data
 
-Continent (VARCHAR) - Name of the Continent
+We are just waiting for DPDP act final date from government side which was supposed to apply from Aug
 
-Country (VARCHAR) - Name of the country for which details are given
 
-Population (INTEGER) - Total population of the country
 
-Affected Population (INTEGER) - Total population which is affected by hunger issues
+Regards
 
-Message
+Pratibha
