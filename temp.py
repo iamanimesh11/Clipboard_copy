ae02057bf983d989b957eec5371dbb22bfdfb160
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-09-05 09:46:49 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,11 +1,36 @@
-Dear Animesh
+Dear Young a kim,
 
 
 
-We are just waiting for DPDP act final date from government side which was supposed to apply from Aug
+I hope you are doing well.
 
 
 
-Regards
+First of all, thanks for writing the mail regarding this project status.
 
-Pratibha
+Last year before using cloud pc we have worked on NQVM but it's very slow and problematic so that's we requested team for cloud pc so in short I prefer to use cloud pc 
+
+for any project development .
+
+
+
+Now comes to the point of windows -windows legacy mode ,As we are facing issue in library mismatch issue and depdnecies problems and if nothing works out then have to create the project from scratch in winodws mode and with the versions of packages you mentioned on RPA portal .
+
+
+
+We are currentyl having another project named as "daily-stokc-report" which in tested phase from your side ,this project is built on top of windows version and so if it piblished to orchestraotr without any errors then we will proceed to development of "shipment tracking project " from scratch with the smae package version used in project "Daily-stock-report"
+
+Also as i already raised concern about slowness and system crashes in windows mode ,I don't have any fix time for you now by which project may be complete .
+
+so even small changes crashes the uipath so it will be a headache at first but will mange at cost of time .
+
+
+
+hope you understand and thansk for your support
+
+
+
+Best Regards,
+
+Animesh
+
