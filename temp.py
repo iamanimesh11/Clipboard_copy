346176daf2f1dbf17acae0ae0f97dd2a79cecc3c
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-09-05 15:11:35 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,17 @@
 
+Respected Ma'am,
+
+
+
+Actually, there is no known source of issue right now.
+
+Self-RPA-HELPDESK told me that UIPATH windows version is still upgrading, and they don't have any solution for unexpected crashes of UiPath while developing process in Windows version. Also, no known way is there to debug what's difference in bot-pc and cloud-pc environment.
+
+In the meantime, A unexpected system exception happens randomly for which I'll get in touch with Preetam Sir. In a case if it resolves the problem
+
+
+
+Best regards,
+
+animesh 
+
