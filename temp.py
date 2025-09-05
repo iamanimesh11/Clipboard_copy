2fea82c5b5b4806de33ea1b6d348cec06ae2477a
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-09-05 16:05:02 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,1 @@
-Respected Ma’am,
 
-Currently, no root cause has been identified for the issue.
-
-The Self-RPA-HELPDESK team informed me that the UiPath Windows version is still under upgrade. At present, they don’t have a solution for the unexpected crashes occurring while developing processes in the Windows environment. Additionally, there is no known way to debug the differences between the bot-PC and cloud-PC environments.
-
-Meanwhile, the unexpected system exception continues to occur intermittently. Preetam Sir suggested that I try reinstalling UiPath once to see if it resolves the issue, so I will check this.
-
-Best regards,
-Animesh
