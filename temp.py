5f7eeacefdef7779c209fad1655e835c6f780d36
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-10-14 14:30:01 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,86 +1,24 @@
--- Create the tables first
-CREATE TABLE users (
-    user_id INT PRIMARY KEY,  signup_date DATE NOT NULL
-);
 
-CREATE TABLE orders (
-    order_id INT PRIMARY KEY,user_id INT NOT NULL,order_date DATE NOT NULL,
-    amount NUMERIC(10, 2) NOT NULL,FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
--- Insert data into the tables
-INSERT INTO users (user_id, signup_date) VALUES (101, '2024-01-01'), (102, '2024-01-10'),(103, '2024-02-15');
-INSERT INTO orders (order_id, user_id, order_date, amount) VALUES (1, 101, '2024-01-05', 200),(2, 101, '2024-02-10', 300),(3, 102, '2024-01-15', 150),
-(4, 103, '2024-03-01', 500),(5, 101, '2024-03-20', 250);
+-- 2. Behavioral Funnels:
+-- For each user, sequence their events.]
+-- Identify users who dropped off at each funnel step.
+-- Compute time spent between steps.
+WITH CTE AS(
+select user_id,event_type,event_time,
+case when event_type ='browse' then 1 
+when event_type='add_to_cart' then 2 
+when event_type ='purchase' then 3 
+end as  rn ,
 
-with cte as(
-select u.*,o.order_date,o.amount from users u join orders o on u.user_id=o.user_id
-),
-
-cte2 as (
-select *,
-LAG(order_date) over (partition by user_id order by order_date) as prev_Date,
-DATEDIFF(order_date,LAG(order_date) over (partition by user_id order by order_date)) as days 
-from cte
+TIMESTAMPdiff(minute,lag(event_time) over (partition by user_id 
+                order by field(event_type,'browse','add_to_cart','purchase')
+),event_time ) as time_Diff
+from user_events
 )
 
-select user_id,count(order_date) as total_orders,sum(amount) as total_Spend,avg(days),max(order_date),
-case when DATEDIFF(CURDATE(),max(order_date)) >60 then "high risk" else "Active" end as churn_risk_flag ,
-case when sum(amount)>= 1000 then 'Gold' when sum(amount)<=500 then "silver" 
-else 'Bronze' end as loyaly_tier
-from cte2
-group by user_id
-
-
-
-
-
-CREATE TABLE users (
-    user_id INT PRIMARY KEY,signup_date DATE );
-
--- Insert data into the 'users' table
-INSERT INTO users (user_id, signup_date) VALUES
-(101, '2024-01-15'),(102, '2024-02-05'),(103, '2024-02-28'),(104, '2024-03-10');
-
--- Create the 'orders' table
-CREATE TABLE orders (
-    order_id INT PRIMARY KEY,user_id INT,order_date DATE,amount DECIMAL(10, 2),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-
--- Insert data into the 'orders' table
-INSERT INTO orders (order_id, user_id, order_date, amount) VALUES
-(1, 101, '2024-01-20', 100),(2, 101, '2024-02-10', 120),(3, 101, '2024-03-05', 200),(4, 102, '2024-02-10', 80),
-(5, 102, '2024-03-12', 120),(6, 103, '2024-03-15', 90),(7, 104, '2024-03-18', 150),(8, 104, '2024-04-20', 160),
-(9, 104, '2024-05-22', 200);
-
-with user_cohort as (
-select user_id,
-signup_date,
-DATE_format(signup_date,'%Y-%m') as cohort_month from users
-)
-,user_orders as (
-select  o.user_id,o.order_date ,uc.cohort_month,
-timestampdiff(Month,uc.signup_date,o.order_date) as months_since_signup
-from orders o  join user_cohort uc on uc.user_id=o.user_id
-),
-
-cohort_activity as (
-select cohort_month,months_since_signup,
-count(distinct user_id) as active_users 
-from user_orders group by cohort_month,months_since_signup
-),
-
-cohort_size as (
-select cohort_month,
-count(distinct user_id) as  total_users
-from user_cohort
-group by cohort_month
-)
-
-select ca.*, cs.total_users,
-round((ca.active_users / cs.total_users) * 100, 2) as retention_Date_percent
-from cohort_activity ca
-join cohort_size cs
-on ca.cohort_month = cs.cohort_month
-
-
+SELECT user_id,
+case when 1 not in (select rn where user_id=user_id) then 'browse',
+case when 2 not in (select rn where user_id=user_id) then 'add_to_Cart',
+case when 3 not in (select rn where user_id=user_id) then 'purchase'
+end as c 
+from cte 
