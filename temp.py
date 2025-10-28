968c0cfa027f221edcb3f5add267272063e7ef7f
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-10-28 16:04:08 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,33 +1,9 @@
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-
-        # 1️⃣ Recursively sort both halves
-        merge_sort(left_half)
-        merge_sort(right_half)
-
-        # 2️⃣ Merge them back
-        i = j = k = 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-
-        # 3️⃣ Copy remaining elements
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
-
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
-
-    return arr
+def examples(n):
+    if n==0:
+        print("base case reached")
+        return
+    print("before recursion:",n)
+    examples(n-1)
+    print("after recursion:",n)
+    
+examples(3)
