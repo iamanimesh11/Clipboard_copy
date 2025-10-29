3115e163a31a20dbe73e6e6b7b4bd979b4b90545
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-10-29 14:45:36 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,34 +1,15 @@
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-        # 1️⃣ Recursively sort both halves
-        merge_sort(left_half)
-        merge_sort(right_half)
-        # 2️⃣ Merge them back
-        i = j = k = 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-        # 3️⃣ Copy remaining elements
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
-    return arr
+def quick_sort(arr):
+    if len(arr)<=1:
+        return arr
     
-my_array = [5,1,4,2,8]
-sorted_array = merge_sort(my_array)
+    pivot = arr[len(arr) // 2]  # Choosing the middle element as pivot
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    
+    return quick_sort(left)+middle+quick_sort(right)
+my_array = [5,1,2,8,4]
+sorted_array = quick_sort(my_array)
 print(f"Original array: {my_array}")
 print(f"Sorted array: {sorted_array}")
 print("HURRAY!!!!")
