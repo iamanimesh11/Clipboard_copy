becb75b1decfb1f031cead75ea4682fe31ce3a85
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-11-06 16:04:39 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,16 +1,7 @@
-def count_Even(arr,count):
-    print(f"arr,calling {arr}")
-    if not arr:
-        return 
-    if len(arr)==1 and arr[0]%2==0:
-        count+=1
-        return
-    print(arr)
-    print(count)
-    if arr[0]%2==0:
-        count+=1
-    return count_Even(arr[1:],count)
-arr=[2,4,3,5,8]
-count=0
-x=count_Even(arr,count)
-print(x)
+def rev(Str):
+    if len(Str)<0:
+        return "" 
+    print(Str)
+    return Str[len(Str)-1]+rev(Str[::-1])
+
+rev("animesh")
